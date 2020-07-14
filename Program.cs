4dbb56068b7a2e3650d891b54126e0d448220db9
Nihad Amirjanli <54079962+nyhead@users.noreply.github.com>
2020-07-14 19:06:26 +0400

    Update Program.cs
    Fixed bunch of bugs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Text.RegularExpressions;
 
 using CliWrap;
 using Newtonsoft.Json.Linq;
@@ -20,36 +21,34 @@ namespace Reddit_Video_Downloader_Telegram_Bot
     {
         static ITelegramBotClient bot;
         static HttpClient client = new HttpClient();
-        static string url;
-        static JArray json;
+        static string url, videoFileName, audioFileName;
         static Message editedMessage;
-        enum Mode { RESOLUTION, VIDEO, AUDIO, SOUNDLESS };
+        enum Mode { RESOLUTION, VIDEO, AUDIO, GIF };
         static Mode mode;
 
 
         static void Main()
         {
             //Reddit Video bot token
-            bot = new TelegramBotClient(""); 
+            bot = new TelegramBotClient("");
 
             var me = bot.GetMeAsync().Result;
             Console.WriteLine(
               $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
             );
-            bot.OnMessage += Bot_OnMessage;
+            bot.OnMessage += BotOnMessage;
             bot.OnCallbackQuery += BotOnCallbackQueryReceivedAsync;
             bot.StartReceiving();
             Thread.Sleep(int.MaxValue);
         }
 
-
-        static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        static async void BotOnMessage(object sender, MessageEventArgs e)
         {
             Message message = e.Message;
             Chat chat = e.Message.Chat;
             if (message.Text != null)
             {
-                Console.WriteLine($"Received a text message from {chat.FirstName}(@{chat.Username}) in chat {chat.Id}. It says {message.Text}");
+                Console.WriteLine($"{chat.FirstName}(@{chat.Username}) {chat.Id}: {message.Text}");
 
                 if (message.Text == "/start")
                 {
@@ -61,26 +60,16 @@ namespace Reddit_Video_Downloader_Telegram_Bot
                     {
                         url = message.Text;
 
-                        HttpResponseMessage response = await client.GetAsync(url);
-                        response.EnsureSuccessStatusCode();
-                        url = response.RequestMessage.RequestUri.ToString();
-                        //Console.WriteLine(url);
-
-                        string jsonData = await client.GetStringAsync(url + ".json");
-
                         InlineKeyboardMarkup inlineKeyboardDownloadMode = new InlineKeyboardMarkup(
                                new[]
                                {
                         InlineKeyboardButton.WithCallbackData("GIF"),
-                        InlineKeyboardButton.WithCallbackData("Video"),
-                        InlineKeyboardButton.WithCallbackData("Audio")
+                        InlineKeyboardButton.WithCallbackData("Video")
                                }
                                );
                         editedMessage = await bot.SendTextMessageAsync(chat, "What do you want to download?",
                         replyMarkup: inlineKeyboardDownloadMode
                         );
-                        
-                        json = JArray.Parse(jsonData);
                     }
                     else
                     {
@@ -105,7 +94,7 @@ namespace Reddit_Video_Downloader_Telegram_Bot
             switch (callbackQuery.Data)
             {
                 case "GIF":
-                    mode = Mode.SOUNDLESS;
+                    mode = Mode.GIF;
                     break;
                 case "Video":
                     mode = Mode.RESOLUTION;
@@ -160,104 +149,211 @@ namespace Reddit_Video_Downloader_Telegram_Bot
                 //mode = Mode.VIDEO;
             }
             else if (mode == Mode.VIDEO)
-            {          
+            {
                 await bot.EditMessageTextAsync(chatId, editedMessage.MessageId, "Wait for the video to be downloaded and sent to you"
                         );
 
-                string videoUrl = json[0]["data"]["children"][0]["data"]["url"].ToString() + '/';
-                string audioUrl = json[0]["data"]["children"][0]["data"]["url"].ToString() + "/audio";
-                string title = json[0]["data"]["children"][0]["data"]["title"].ToString();
-                string dashUrl = json[0]["data"]["children"][0]["data"]["media"]["reddit_video"]["dash_url"].ToString();
-             
-                string videoFileName = $"video_{chatId}.mp4";
-                string audioFileName = $"audio_{chatId}.mp3";
+                bool downloadResultVideo = await DownloadVideo(url, callbackQuery.Data);
 
-                bool downloadResultDash = await Download(dashUrl, "DASHPlaylist.mpd");
-                bool downloadResultAudio = await Download(audioUrl, audioFileName);
-
-                if(downloadResultDash)
+                if (downloadResultVideo)
                 {
+                    using (var stream = System.IO.File.OpenRead("muxed_" + videoFileName))
+                    {
+                        await bot.SendVideoAsync(chatId, stream);
+                    }
+                    System.IO.File.Delete("muxed_" + videoFileName);                   
+                }
+                else if(!downloadResultVideo && System.IO.File.Exists(videoFileName))
+                {
+                    using (var stream = System.IO.File.OpenRead(videoFileName))
+                    {
+                        await bot.SendVideoAsync(chatId, stream);
+                    }
+                    System.IO.File.Delete(videoFileName);        
+                }
+                else
+                {
+                    await bot.SendTextMessageAsync(chatId, "Url is not supported");
+                }
+            }
+            else if (mode == Mode.GIF)
+            {
+
+                await bot.EditMessageTextAsync(chatId, editedMessage.MessageId, "Wait for the gif to be downloaded and sent to you"
+                        );
+
+                string videoUrl, dashUrl;
+                videoUrl = dashUrl = String.Empty;
+
+                string ptrn1 = @"(?<url>https?://(?:[^/]+\.)?reddit\.com/r/[^/]+/comments/[^/?#&]+)";
+                string ptrn2 = @"https?://v\.redd\.it/(?<id>[^/?#&]+)";
+
+                if (Regex.IsMatch(url, ptrn1))
+                {
+                    string jsonData = await client.GetStringAsync(Regex.Match(url, ptrn1).Value + "/.json");
+                    JArray json = JArray.Parse(jsonData);
+
+                    videoUrl = json[0]["data"]["children"][0]["data"]["url"].ToString() + '/';
+                    dashUrl = videoUrl + "DASHPlaylist.mpd";
+                    bool downloadResultDash = await Download(dashUrl, "DASHPlaylist.mpd");
+
+                    if (downloadResultDash)
+                    {
+    
+                        var doc = new XmlDocument();
+                        doc.Load("DASHPlaylist.mpd");
+
+                        var curr = doc.GetElementsByTagName("Representation");
+
+
+                        foreach (XmlNode node in curr)
+                        {
+                            var attrs = node.Attributes;
+                            var parentAttrs = node.ParentNode.Attributes;
+                            if (attrs["mimeType"]?.Value == "video/mp4" && attrs["height"]?.Value == "480")
+                            {
+                              videoUrl += node.FirstChild.InnerText;
+                            }
+                            else if (parentAttrs["mimeType"]?.Value == "video/mp4" && attrs["height"]?.Value == "480")
+                            {
+                              videoUrl += node.FirstChild.InnerText;
+                            }
+                        }
+                        System.Console.WriteLine(videoUrl);
+                    InputOnlineFile inputOnlineFile = new InputOnlineFile(videoUrl);
+                    await bot.SendVideoAsync(chatId, inputOnlineFile);
+                    }
+                }
+                else if (Regex.IsMatch(url, ptrn2))
+                {
+                    string id = Regex.Match(url, ptrn2).Groups["id"].Value;
+
+                    videoUrl = $"https://v.redd.it/{id}/";
+                    dashUrl = $"https://v.redd.it/{id}/DASHPlaylist.mpd";
+                    bool downloadResultDash = await Download(dashUrl, "DASHPlaylist.mpd");
+
+                    if (downloadResultDash)
+                    {
+    
+                        var doc = new XmlDocument();
+                        doc.Load("DASHPlaylist.mpd");
+
+                        var curr = doc.GetElementsByTagName("Representation");
+
+
+                        foreach (XmlNode node in curr)
+                        {
+                            var attrs = node.Attributes;
+                            var parentAttrs = node.ParentNode.Attributes;
+                            if (attrs["mimeType"]?.Value == "video/mp4" && attrs["height"]?.Value == "480")
+                            {
+                              videoUrl += node.FirstChild.InnerText;
+                            }
+                            else if (parentAttrs["mimeType"]?.Value == "video/mp4" && attrs["height"]?.Value == "480")
+                            {
+                              videoUrl += node.FirstChild.InnerText;
+                            }
+                        }
+                        System.Console.WriteLine(videoUrl);
+                        InputOnlineFile inputOnlineFile = new InputOnlineFile(videoUrl);
+                        await bot.SendVideoAsync(chatId, inputOnlineFile);
+                    }
+                }
+                else
+                {
+                    await bot.SendTextMessageAsync(chatId, "Url is not supported");
+                }
+                
+            }
+
+        }
+         static async Task<bool> DownloadVideo(string url, string resolution)
+        {
+            string videoUrl, audioUrl, dashUrl;
+            videoUrl = audioUrl = dashUrl = String.Empty;
+            string ptrn1 = @"(?<url>https?://(?:[^/]+\.)?reddit\.com/r/[^/]+/comments/(?<id>[^/?#&]+))";
+            string ptrn2 = @"https?://v\.redd\.it/(?<id>[^/?#&]+)";
+
+            if (Regex.IsMatch(url, ptrn1))
+            {
+                string id = Regex.Match(url, ptrn1).Groups["id"].Value;
+
+                videoFileName = "video_" + id + ".mp4";
+                audioFileName = "audio_" + id + ".mp3";
+
+                string jsonData = await client.GetStringAsync(Regex.Match(url, ptrn1).Value + "/.json");
+                JArray json = JArray.Parse(jsonData);
+
+                videoUrl = json[0]["data"]["children"][0]["data"]["url"].ToString() + '/';
+                audioUrl = videoUrl;
+                dashUrl = videoUrl + "DASHPlaylist.mpd";
+
+            }
+            else if (Regex.IsMatch(url, ptrn2))
+            {
+                string id = Regex.Match(url, ptrn2).Groups["id"].Value;
+
+                videoFileName = "video_" + id + ".mp4";
+                audioFileName = "audio_" + id + ".mp3";
+
+                videoUrl = $"https://v.redd.it/{id}/";
+                audioUrl = $"https://v.redd.it/{id}/";
+                dashUrl = $"https://v.redd.it/{id}/DASHPlaylist.mpd";
+            }
+            else
+            {
+                return false;
+            }
+                bool downloadResultDash = await Download(dashUrl, "DASHPlaylist.mpd");
+
+                if (downloadResultDash)
+                {                   
                     var doc = new XmlDocument();
                     doc.Load("DASHPlaylist.mpd");
 
-                    var curr = doc.GetElementsByTagName("Representation");            
+                    var curr = doc.GetElementsByTagName("Representation");
 
-
-                        foreach(XmlNode node in curr)
+                    foreach (XmlNode node in curr)
+                    {
+                        var attrs = node.Attributes;
+                        var parentAttrs = node.ParentNode.Attributes;
+                        if (attrs["mimeType"]?.Value == "video/mp4" && attrs["height"]?.Value == resolution)
                         {
-                            var attrs = node.Attributes;
-                            if(attrs.GetNamedItem("mimeType").InnerText == "video/mp4")
-                            {
-                                if(attrs.GetNamedItem("height").InnerText == callbackQuery.Data)
-                                {
-                                    videoUrl += node.FirstChild.InnerText;
-                                }                    
-                            }
-                        }           
-                        System.Console.WriteLine(videoUrl);
-
-                        bool downloadResultVideo = await Download(videoUrl, videoFileName);            
-
-                        if (downloadResultAudio == true && downloadResultVideo == true)
-                        {
-                            string muxedFileName = "muxed_" + videoFileName;
-                            var result = await Cli.Wrap("path to ffmpeg")
-                                        .WithArguments($"-i {videoFileName} -i {audioFileName} {muxedFileName}")
-                                        .ExecuteAsync();
-
-                            if (result.RunTime.TotalSeconds > 0)
-                            {
-                                using (var stream = System.IO.File.OpenRead(muxedFileName))
-                                {
-                                    await bot.SendVideoAsync(chatId, stream, caption: title);
-                                }
-                                System.IO.File.Delete(videoFileName);
-                                System.IO.File.Delete(audioFileName);
-                                System.IO.File.Delete(muxedFileName);
-                            }
+                            videoUrl += node.FirstChild.InnerText;
                         }
-                        else if (downloadResultVideo == true)
+                        else if (attrs["mimeType"]?.Value == "audio/mp4")
                         {
-                            InputOnlineFile inputOnlineFile = new InputOnlineFile(videoUrl);
-                            await bot.SendVideoAsync(chatId, inputOnlineFile, caption: title);
-                            System.IO.File.Delete(videoFileName);
+                           audioUrl += node.ChildNodes[1].InnerText; 
                         }
-                        else
+                        else if (parentAttrs["mimeType"]?.Value == "video/mp4" && attrs["height"]?.Value == resolution)
                         {
-                            await bot.SendTextMessageAsync(chatId, "Request failed. The link has to be a reddit post with video");
+                            videoUrl += node.FirstChild.InnerText;
                         }
-                    
+                        else if (parentAttrs["mimeType"]?.Value == "audio/mp4")
+                        {
+                           audioUrl += node.ChildNodes[1].InnerText; 
+                        }
+                    }
+                    System.Console.WriteLine(videoUrl);
+                    System.Console.WriteLine(audioUrl);
 
-                }
-            }
-            else if (mode == Mode.AUDIO)
-            {
+                    bool downloadResultVideo = await Download(videoUrl, videoFileName);
+                    bool downloadResultAudio = await Download(audioUrl, audioFileName);
 
-                await bot.EditMessageTextAsync(chatId, editedMessage.MessageId,
-                    "Wait for the audio to be downloaded and sent to you");
+                    if (downloadResultAudio == true && downloadResultVideo == true)
+                    {
+                        string muxedFileName = "muxed_" + videoFileName;
+                        var result = await Cli.Wrap("/usr/bin/ffmpeg")
+                                    .WithArguments($"-y -i {videoFileName} -i {audioFileName} {muxedFileName}")
+                                    .ExecuteAsync();
+                        System.IO.File.Delete(videoFileName);
+                        System.IO.File.Delete(audioFileName);
+                        return true;
+                    }
+                }            
 
-                string audioUrl = json[0]["data"]["children"][0]["data"]["url"].ToString() + "/audio";
-                string title = json[0]["data"]["children"][0]["data"]["title"].ToString();
-
-                //Uploading file by passing Url
-                InputOnlineFile inputOnlineFile = new InputOnlineFile(audioUrl);
-                await bot.SendAudioAsync(chatId, inputOnlineFile, caption: title);
-
-            }
-            else if (mode == Mode.SOUNDLESS)
-            {
-
-                await bot.EditMessageTextAsync(chatId, editedMessage.MessageId, "Wait for the video to be downloaded and sent to you"
-                        );
-                string videoUrl = json[0]["data"]["children"][0]["data"]["media"]["reddit_video"]["fallback_url"].ToString();
-                string title = json[0]["data"]["children"][0]["data"]["title"].ToString();
-                InputOnlineFile inputOnlineFile = new InputOnlineFile(videoUrl);
-                await bot.SendVideoAsync(chatId, inputOnlineFile, caption: title);
-
-            }
-            
-
-        }
+            return false;
+        }       
         static async Task<bool> Download(string url, string fileName)
         {
             using (HttpResponseMessage response = await client.GetAsync(url))
@@ -277,5 +373,5 @@ namespace Reddit_Video_Downloader_Telegram_Bot
             }
             return false;
         }
-    } 
+    }
 }
